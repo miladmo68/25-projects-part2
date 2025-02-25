161174994a4fa21b7da1ec3d4a2b3a4f1d980f60
miladmo68 <miladmo68@gmail.com>
2025-02-25 16:16:03 -0500

    count down timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import PaginationTest from "./components/1. pagination/PaginationTest";
 import DigitalClock from "./components/2. digital-clock";
+import CountdownTimerTest from "./components/3. countdown-timer/test";
 
 function App() {
   return (
     <div>
       <h1 className="title">25 React JS Interview Projects : Part 2</h1>
-      {/* <PaginationTest /> */}
+      <PaginationTest />
       <DigitalClock />
+      <CountdownTimerTest />
     </div>
   );
 }
---
+++ src/components/3. countdown-timer/test.jsx
@@ -0,0 +1,17 @@
+import CountdownTimer from ".";
+import "./timer.css";
+const CountdownTimerTest = () => {
+  function handleTimeFinish() {
+    console.log(
+      "Once the timer is finished I want to make an api call and save some data to database"
+    );
+  }
+  return (
+    <div className="countdown-timer-container">
+      <h1>CountDown Timer</h1>
+      <CountdownTimer initialTime={120} onTimeFinish={handleTimeFinish} />
+    </div>
+  );
+};
+
+export default CountdownTimerTest;

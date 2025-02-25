848c44163da2c6ad5cedbf20a61a3cabe1cb855f
miladmo68 <miladmo68@gmail.com>
2025-02-25 15:56:40 -0500

    Digital clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import PaginationTest from "./components/1. pagination/PaginationTest";
+import DigitalClock from "./components/2. digital-clock";
 
 function App() {
   return (
     <div>
       <h1 className="title">25 React JS Interview Projects : Part 2</h1>
-      <PaginationTest />
+      {/* <PaginationTest /> */}
+      <DigitalClock />
     </div>
   );
 }

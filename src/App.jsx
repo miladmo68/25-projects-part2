2690b01ed8cc55342051bea0b339acf01026a310
miladmo68 <miladmo68@gmail.com>
2025-02-25 14:59:35 -0500

    01-pagination

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PaginationTest from "./components/1. pagination/PaginationTest";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">25 React JS Interview Projects : Part 2</h1>
+      <PaginationTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/1. pagination/Pagination.jsx
@@ -0,0 +1,39 @@
+const Pagination = ({ currentPage, totalPages = 10, onPageChange }) => {
+  function generateNoOfPages() {
+    const pages = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        className="pagination-btn"
+        disabled={currentPage === 1}
+      >
+        Prev
+      </button>
+      {generateNoOfPages().map((pageNo) => (
+        <button
+          key={pageNo}
+          className="pagination-btn"
+          onClick={() => onPageChange(pageNo)}
+        >
+          {pageNo}
+        </button>
+      ))}
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        className="pagination-btn"
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;

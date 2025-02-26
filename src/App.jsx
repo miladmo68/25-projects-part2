beff873eec471d6f487c841f1f043196abd19ec4
miladmo68 <miladmo68@gmail.com>
2025-02-26 11:01:28 -0500

    Random Quote Generator

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import PaginationTest from "./components/1. pagination/PaginationTest";
 import DigitalClock from "./components/2. digital-clock";
 import CountdownTimerTest from "./components/3. countdown-timer/test";
 import StepProgressBarTest from "./components/4. step-progress-bar/test";
+import RandomQuoteGenerator from "./components/5. random-quote-generator";
 
 function App() {
   return (
     <div>
       <h1 className="title">25 React JS Interview Projects : Part 2</h1>
       <PaginationTest />
-      <DigitalClock />
+      <hr style={{ border: "1px solid black" }} />
+      <DigitalClock />\
+      <hr style={{ border: "1px solid black" }} />
       <CountdownTimerTest />
+      <hr style={{ border: "1px solid black" }} />
       <StepProgressBarTest />
+      <hr style={{ border: "1px solid black" }} />
+      <RandomQuoteGenerator />
+      <hr style={{ border: "1px solid black" }} />
     </div>
   );
 }
---
+++ src/components/5. random-quote-generator/index.jsx
@@ -0,0 +1,53 @@
+//https://api.quotable.io/quotes/random
+
+import React, { useEffect, useState } from "react";
+import "./quote.css";
+
+const RandomQuoteGenerator = () => {
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState(null);
+
+  async function fetchQuote() {
+    try {
+      setLoading(true);
+      const res = await fetch("https://api.quotable.io/quotes/random");
+      const data = await res.json();
+      //   console.log(data);
+
+      if (data && data.length > 0) {
+        setLoading(false);
+        setQuote(data[0]);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  function handleRefresh() {
+    fetchQuote();
+  }
+
+  if (loading) {
+    return <h3>Loading Quote ! Please wait</h3>;
+  }
+
+  return (
+    <div className="random-quote-generator">
+      <h1>Random Quote Generator</h1>
+      <div className="quote-wrapper">
+        <p>{quote?.author}</p>
+        <p>{quote?.content}</p>
+      </div>
+      <button className="refresh-btn" onClick={handleRefresh}>
+        Refresh
+      </button>
+    </div>
+  );
+};
+
+export default RandomQuoteGenerator;

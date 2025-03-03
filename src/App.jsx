fcdba9baa3964cc1311b7fc49156ad55b5872ee7
miladmo68 <miladmo68@gmail.com>
2025-03-03 13:48:32 -0500

    BMI Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import PaginationTest from "./components/1. pagination/PaginationTest";
 import MusicPlayer from "./components/10. music-player";
 import ProgressBar from "./components/11. progress-bar";
+import BMICalculator from "./components/12. bmi-calculator";
 import DigitalClock from "./components/2. digital-clock";
 import CountdownTimerTest from "./components/3. countdown-timer/test";
 import StepProgressBarTest from "./components/4. step-progress-bar/test";
@@ -36,6 +37,8 @@ function App() {
       <MusicPlayer />
       <hr style={{ border: "1px solid black" }} />
       <ProgressBar />
+      <hr style={{ border: "1px solid black" }} />
+      <BMICalculator />
     </div>
   );
 }

445d4c38cc2fe83ec6c4b51c6bfa597edb8eac77
miladmo68 <miladmo68@gmail.com>
2025-03-19 13:40:57 -0400

    Nested Comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import DragAndDropFeature from "./components/14. drag-and-drop";
 import FormValidation from "./components/15. simple-form-validation";
 import FileUpload from "./components/16. file-upload";
 import Quiz from "./components/17. quiz-app";
+import NestedComments from "./components/18. nested-comments";
 import DigitalClock from "./components/2. digital-clock";
 import CountdownTimerTest from "./components/3. countdown-timer/test";
 import StepProgressBarTest from "./components/4. step-progress-bar/test";
@@ -54,6 +55,8 @@ function App() {
       <FileUpload />
       <hr style={{ border: "1px solid black" }} />
       <Quiz />
+      <hr style={{ border: "1px solid black" }} />
+      <NestedComments />
     </div>
   );
 }
